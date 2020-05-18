c8663b7ea6b0707f46bcda5ddaea3afbcf22bf23
Tamas Ujj <bgunne@gmail.com>
2020-05-18 14:42:08 +0200

    Updates

---
+++ precache-manifest.733e4cfc9df8853c84d5ac88fa504ce1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b56dd5bc10b795dcae7bc657c19239",
+    "url": "/pizzavilag/index.html"
+  },
+  {
+    "revision": "9f6ac9e1b02f3aef6e65",
+    "url": "/pizzavilag/static/css/2.7ba1dc46.chunk.css"
+  },
+  {
+    "revision": "22fe4e4260d6ca37ae94",
+    "url": "/pizzavilag/static/css/main.163b4946.chunk.css"
+  },
+  {
+    "revision": "9f6ac9e1b02f3aef6e65",
+    "url": "/pizzavilag/static/js/2.1dfd8151.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pizzavilag/static/js/2.1dfd8151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22fe4e4260d6ca37ae94",
+    "url": "/pizzavilag/static/js/main.fdfdb595.chunk.js"
+  },
+  {
+    "revision": "38099e9a848105170c24",
+    "url": "/pizzavilag/static/js/runtime-main.fae46e9b.js"
+  },
+  {
+    "revision": "f34d0ef124b36667c76b6d4fccd0e189",
+    "url": "/pizzavilag/static/media/header.f34d0ef1.jpg"
+  }
+]);
